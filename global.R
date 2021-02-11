919bb7cedb99b6faa13f8871f9dec0dc084cb50e
marcelo.gadelha <marcelo.gadelha@cidadania.gov.br>
2021-02-10 22:30:08 -0300

    Estrutura Básica

---
+++ global.R
@@ -0,0 +1,29 @@
+#
+#
+#SOFTWARE PARA LEITURA DE URL E GERAÇÃO DE PAINEL POADRÃO
+#
+#
+
+#
+# FORMATO DO ARQUIVO
+# CODIGO_IBGE_6;VALOR
+#
+
+
+#
+# PACOTES NECESSÁRIOS
+#
+
+#install.packages("shiny")
+#install.packages("dplyr")
+#install.packages("plotly")
+
+
+#
+# PACOTES
+#
+
+library("shiny")
+library("dplyr")
+library("plotly")
+
